de8834b3366b040cabab7d390a4a02964e6d0774
ei17ringo <ei17ringo@gmail.com>
2016-10-16 21:53:52 +0800

    commit init

---
+++ sampleSlider/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  sampleSlider
+//
+//  Created by Eriko Ichinohe on 2016/01/27.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        print(sender.value)
+        
+        if sender.value <= 0.5 {
+            print("音が小さいです")
+        }else{
+            print("音が大きいです")
+        }
+    }
+
+}
+
